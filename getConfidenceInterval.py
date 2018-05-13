531e813f394da80f920a3397bd69ccf332a53166
Wok <woctezuma@users.noreply.github.com>
2018-05-13 11:09:55 +0200

    Refactoring

--- getConfidenceInterval.py
+++ getConfidenceInterval.py
@@ -3,36 +3,38 @@
 from math import ceil, floor, sqrt
 from random import randint
 
-def getConfidenceInterval(num_people,
-                          num_iter = 1000000,
-                          percentile = 2.576,
-                          precision = 1e5,
-                          num_days = 365):
-   '''
-   Compute a 99%-confidence interval for consecutive birthdays probability
-   '''
-   mean = 0.0
-   variance = 0.0 # not exactly
-   for i in range(1, num_iter+1):
-      x = [randint(1, num_days) for person in xrange(num_people)]
-      x.sort()
-      is_consecutive = any(p+1==q for (p,q) in zip(x[:-1], x[1:]))
-      is_a_loop = (x[0]+num_days-1 == x[-1])
-      is_positive = int(is_consecutive or is_a_loop)
-      delta = is_positive-mean
-      mean += delta/float(i)
-      variance += delta*(is_positive-mean)
-   sd = sqrt(variance/float(num_iter-1))
-   lower_bound = mean - percentile*sd/sqrt(num_iter)
-   upper_bound = mean + percentile*sd/sqrt(num_iter)
-   print("Number of people:"),
-   print(num_people),
-   print("\tLower bound:"),
-   print(floor(lower_bound*precision)/precision),
-   print("\tUpper bound:"),
-   print(ceil(upper_bound*precision)/precision)
-   return (lower_bound, upper_bound)
+
+def get_confidence_interval(num_people,
+                            num_iter=1000000,
+                            percentile=2.576,
+                            precision=1e5,
+                            num_days=365):
+    """
+    Compute a 99%-confidence interval for consecutive birthdays probability
+    """
+    mean = 0.0
+    variance = 0.0  # not exactly
+    for i in range(1, num_iter + 1):
+        x = [randint(1, num_days) for person in range(num_people)]
+        x.sort()
+        is_consecutive = any(p + 1 == q for (p, q) in zip(x[:-1], x[1:]))
+        is_a_loop = (x[0] + num_days - 1 == x[-1])
+        is_positive = int(is_consecutive or is_a_loop)
+        delta = is_positive - mean
+        mean += delta / float(i)
+        variance += delta * (is_positive - mean)
+    sd = sqrt(variance / float(num_iter - 1))
+    lower_bound = mean - percentile * sd / sqrt(num_iter)
+    upper_bound = mean + percentile * sd / sqrt(num_iter)
+    print("Number of people:"),
+    print(num_people),
+    print("\tLower bound:"),
+    print(floor(lower_bound * precision) / precision),
+    print("\tUpper bound:"),
+    print(ceil(upper_bound * precision) / precision)
+    return lower_bound, upper_bound
+
 
 if __name__ == "__main__":
-   for num_people in range(1, 18):
-      (lower_bound, upper_bound) = getConfidenceInterval(num_people)
+    for num_of_people in range(1, 18):
+        (lb, ub) = get_confidence_interval(num_of_people)
--- getSolution.py
+++ getSolution.py
@@ -5,6 +5,7 @@
 
 from math import pow
 
+
 def choose(n, k):
     """
     A fast way to calculate binomial coefficients by Andrew Dalke (contrib).
@@ -12,7 +13,7 @@ def choose(n, k):
     if 0 <= k <= n:
         ntok = 1
         ktok = 1
-        for t in xrange(1, min(k, n - k) + 1):
+        for t in range(1, min(k, n - k) + 1):
             ntok *= n
             ktok *= t
             n -= 1
@@ -20,52 +21,55 @@ def choose(n, k):
     else:
         return 0
 
-def getMyGuess(num_people,
-               precision = 1e5,
-               num_days = 365):
-    '''
+
+def get_my_guess(num_people,
+                 precision=1e5,
+                 num_days=365):
+    """
     Compute my initial guess of the consecutive birthdays probability
-    '''
-    left_result = 1 - (num_people-1)/float(2*num_days+num_people-1)
+    """
+    left_result = 1 - (num_people - 1) / float(2 * num_days + num_people - 1)
     right_result = 1
     for i in range(num_people):
-        right_result *= (num_days-2*i)/float(num_days)
-    my_guess = left_result*(1-right_result)
+        right_result *= (num_days - 2 * i) / float(num_days)
+    guess = left_result * (1 - right_result)
     print("Number of people:"),
     print(num_people),
     print("\tMy guess:"),
-    print(round(my_guess*precision)/precision)
-    return my_guess
+    print(round(guess * precision) / precision)
+    return guess
 
-def getCorrectSolution(num_people,
-                       precision = 1e5,
-                       num_days = 365,
-                       limit_overflow = 1e42):
-    '''
+
+def get_correct_solution(num_people,
+                         precision=1e5,
+                         num_days=365,
+                         limit_overflow=1e42):
+    """
    Compute the consecutive birthdays probability
-   '''
+   """
     result = 0
-    for k in range(1, num_people+1):
-        my_product = 1/float(k*pow(num_days, num_people-k))
+    for k in range(1, num_people + 1):
+        my_product = 1 / float(k * pow(num_days, num_people - k))
         for i in range(1, k):
-            my_product *= (num_days-(k+i))/float(num_days*i)
+            my_product *= (num_days - (k + i)) / float(num_days * i)
         my_sum = 0
-        for j in range(0, k+1):
-            my_sum += pow(-1, j) * choose(k, j) * pow(k-j, num_people)
-        result += my_product*my_sum
-        if my_sum>limit_overflow:
+        for j in range(0, k + 1):
+            my_sum += pow(-1, j) * choose(k, j) * pow(k - j, num_people)
+        result += my_product * my_sum
+        if my_sum > limit_overflow:
             raise OverflowError
-    probability = 1-result
+    proba = 1 - result
     print("Number of people:"),
     print(num_people),
     print("\tProbability:"),
-    print(round(probability*precision)/precision)
-    return probability
+    print(round(proba * precision) / precision)
+    return proba
+
 
 if __name__ == "__main__":
-    for num_people in range(1, 34):
+    for num_of_people in range(1, 34):
         try:
-            probability = getCorrectSolution(num_people)
-            my_guess = getMyGuess(num_people)
+            probability = get_correct_solution(num_of_people)
+            my_guess = get_my_guess(num_of_people)
         except OverflowError:
             break
