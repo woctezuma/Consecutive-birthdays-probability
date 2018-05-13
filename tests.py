c0aafaa19bd7f260083beab28e6b72f40e6a51f2
Wok <woctezuma@users.noreply.github.com>
2018-05-13 11:18:42 +0200

    Unit tests

---
+++ tests.py
@@ -0,0 +1,20 @@
+import unittest
+
+import get_confidence_interval
+import get_solution
+
+
+class TestGetConfidenceIntervalMethods(unittest.TestCase):
+
+    def test_main(self):
+        self.assertTrue(get_confidence_interval.main())
+
+
+class TestGetSolutionMethods(unittest.TestCase):
+
+    def test_main(self):
+        self.assertTrue(get_solution.main())
+
+
+if __name__ == '__main__':
+    unittest.main()
